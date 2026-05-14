3edeed3c4b97e7b4c304098f19b6bef62394eaaf
AlecForbes <alecforbesguitar@gmail.com>
2026-05-15 10:36:29 +1200

    Added first component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,14 @@
+<script>
+    let name='Alec';
+
+</script>
+
+<div></div>
+<p>{name} ipsum dolor sit amet consectetur adipisicing elit. Sed exercitationem, iure rem voluptatibus quis enim fugiat recusandae quod sapiente, ducimus fugit vel sit voluptatem facere laborum odit aliquid quisquam. Rerum.</p>
+
+<style>
+    p{
+        color:hotpink;
+
+    }
+</style>
